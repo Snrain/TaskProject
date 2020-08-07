029efb02ff53eba42ef681649dff45bd41a9488b
lwk <1091807686@qq.com>
2020-08-07 15:22:22 +0800

    在初始页加了个跳转

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './index.less';
+import { Link } from 'react-router-dom';
 
 export default () => {
   title: 'hi';
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <Link to="/market">toMarket</Link>
     </div>
   );
-}
+};
